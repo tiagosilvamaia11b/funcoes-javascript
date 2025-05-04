020355d979bc0668f7c050bc6bbde70a054bb225
tmaia5003 <tmaia5003@gmail.com>
2025-05-04 20:52:11 +0000

    resolução do exercicio 1001 - extremamente básico

---
+++ pages/extremamente-basico/extremamente-basico.js
@@ -0,0 +1,12 @@
+function somar(a, b) {
+    let x = a + b
+    return x
+}
+
+let soma1 = somar(10, 9)
+let soma2 = somar(-10, 4)
+let soma3 = somar(15, -7)
+
+console.log(`X1 = ${soma1}`)
+console.log(`X2 = ${soma2}`)
+console.log(`X3 = ${soma3}`)
